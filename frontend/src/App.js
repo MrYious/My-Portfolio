c65671845be087c113998b13d5e3e54d638b04ee
MrYious <rosariomark37@gmail.com>
2022-03-27 11:16:00 +0800

    feat: add theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CssBaseline } from '@mui/material';
+import { ThemeModeContext } from './providers/ThemeModeContext';
+import { useState } from 'react';
+
+export default function App() {
+  //State for the current mode [dark or light]
+  const [mode, setMode] = useState('light');
+
+  //Object for the value of the ThemeModeContext
+  const themeMode = {
+    toggleThemeMode: () => {
+      setMode((prev) => prev === 'light'? 'dark': 'light')
+    }
+  };
+
+  //Theme for MUI ThemeContext
+  const theme = createTheme({
+    palette: {
+      mode,
+      ...( mode === 'light'
+      ? {
+        // Light Mode
+        primary: {main: '#00796b',
+        },
+        secondary: {
+          main: 'rgb(220, 0, 78)',
+        },
+        background: {
+          default: '#fff',
+          paper: '#fff',
+        },
+      }
+      : {
+          // Dark Mode
+          primary: {
+            main: '#64ffda',
+          },
+          secondary: {
+            main: '#311b92',
+          },
+          background: {
+            default: '#212121',
+            paper: '#424242',
+          },
+        }
+      )
+    }
+  });
+
+  return (<>
+    <ThemeModeContext.Provider value={themeMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
+    </ThemeModeContext.Provider>
+  </>);
 }
 
-export default App;
